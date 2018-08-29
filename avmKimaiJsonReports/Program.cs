41554ec2846b993595050de9258d2cf4cd45bec3
Andre Mesquita <andre.mesquita@impeto.com.br>
2018-08-29 11:00:39 -0300

    Kimai Integration to generate reports

---
+++ avmKimaiJsonReports/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace avmKimaiJsonReports
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static async Task<object> Reports()
+        {
+            HttpClient client = new HttpClient();
+
+            string user = "";
+            string pass = "";
+
+            string jsonString = "{\"method\":\"authenticate\", \"jsonrpc\":\"2.0\",\"id\":\"1\",\"params\":[\"{0}\",\"{1}\"]}";
+            jsonString = string.Format(jsonString, user, pass);
+
+            string url = "http://example.com/core/json.php";
+
+            StringContent sc = new StringContent(jsonString, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync(url, sc);
+            string result = await response.Content.ReadAsStringAsync();
+
+            return result;
+        }
+    }
+}
---
+++ avmKimaiReports/Program.cs
@@ -0,0 +1,45 @@
+﻿using avmKimaiSoapReports.KimaiService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace avmKimaiReports
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string apiKey = string.Empty;
+
+            var client = new Kimai_Remote_ApiService();
+            client.Url = "https://demo.kimai.org/core/soap.php";
+
+            var apiToken = client.authenticate("admin", "changeme").ToList();
+            var elements = apiToken.Where(x => x.GetType().ToString().Equals("System.Xml.XmlElement")).ToList();
+            
+            foreach (var item in elements)
+            {
+                if (item.GetType().ToString() == "System.Xml.XmlElement")
+                {
+                    var elemento = (System.Xml.XmlElement)item;
+                    if (elemento.InnerText.Contains("itemsapiKey"))
+                    {
+                        apiKey = elemento.InnerText.Replace("itemsapiKey", "").ToString();
+                        break;
+                    }                    
+                }
+            }            
+
+            var projects = client.getProjects(apiKey,null).ToList();
+            var active = client.getActiveRecording(apiKey);
+            var customers = client.getCustomers(apiKey);
+            int projectId = 1;
+            var tasks = client.getTasks(apiKey, projectId);
+            var users = client.getUsers(apiKey);            
+        }
+    }
+}
